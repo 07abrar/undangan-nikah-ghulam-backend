19794121c43dd5fd2af9e08f0eaf1f29fce8fc18
Ghulam Abrar <gh_abrar51@yahoo.co.id>
2026-03-29 06:39:56 +0900

    Add API health-check

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+def hello(request):
+    """Health-check endpoint to confirm the API is alive."""
+    return Response({"message": "Hello from undangan-nikah-ghulam-backend!"})
