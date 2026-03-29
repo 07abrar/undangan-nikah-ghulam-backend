69075f97b0f894bbb22ebab94d692430e73e9e2b
Ghulam Abrar <gh_abrar51@yahoo.co.id>
2026-03-30 01:03:16 +0900

    Define Couple and Event models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Couple(models.Model):
+    groom_name = models.CharField(max_length=100)
+    bride_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.groom_name} & {self.bride_name}"
+
+    class Meta:
+        verbose_name_plural = "Couple"
+
+
+class Event(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.date}"
